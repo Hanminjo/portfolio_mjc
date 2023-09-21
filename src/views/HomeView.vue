cef734558e6412658b67fdbdbad39ce02431d941
johanmin <johanmin@itc.mjc.ac.kr>
2023-09-21 10:17:23 +0900

    change image path

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,14 @@
   <div>
     <div id="top">
       <swiper class="swiper" :slides-per-view="1" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
-        <swiper-slide style="background-image:url(/top.1.jpg)" class="slide">
+        <swiper-slide style="background-image:url(/portfolio_mjc/top.1.jpg)" class="slide">
 
           <div class="header">명지전문대 정보통신공학과 </div>
         </swiper-slide>
-        <swiper-slide style="background-image:url(/top.1.jpg)" class="slide">
+        <swiper-slide style="background-image:url(/portfolio_mjc/top.1.jpg)" class="slide">
           <div class="header">명지전문대 정보통신공학과 </div>
         </swiper-slide>
-        <swiper-slide style="background-image:url(/top.1.jpg)" class="slide">
+        <swiper-slide style="background-image:url(/portfolio_mjc/top.1.jpg)" class="slide">
           <div class="header">명지전문대 정보통신공학과 </div>
         </swiper-slide>
 
@@ -20,11 +20,12 @@
       <div class="list">
         <div class="item" @click="movePage('/portfolio1')" data-aos="zoom-out-right"
           style="background-image:url(/portfolio_mjc/top.1.jpg)"></div>
-        <div class="item" data-aos="zoom-in-left" style="background-image:url(/top.1.jpg)"></div>
-        <div class="item" data-aos="fade-up" data-aos-duration="3000" style="background-image:url(/top.1.jpg)"></div>
-        <div class="item" style="background-image:url(/top.1.jpg)"></div>
-        <div class="item" style="background-image:url(/top.1.jpg)"></div>
-        <div class="item" style="background-image:url(/top.1.jpg)"></div>
+        <div class="item" data-aos="zoom-in-left" style="background-image:url(/portfolio_mjc/top.1.jpg)"></div>
+        <div class="item" data-aos="fade-up" data-aos-duration="3000"
+          style="background-image:url(/portfolio_mjc/top.1.jpg)"></div>
+        <div class="item" style="background-image:url(/portfolio_mjc/top.1.jpg)"></div>
+        <div class="item" style="background-image:url(/portfolio_mjc/top.1.jpg)"></div>
+        <div class="item" style="background-image:url(/portfolio_mjc/top.1.jpg)"></div>
 
 
 
