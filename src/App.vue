fa817686cccabec238b8b7f21a905366e4772d49
Hanminjo <johanmin@itc.mjc.ac.kr>
2023-10-19 19:06:34 +0900

    페이지별추가

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,73 @@
 <template>
   <v-app>
+    <v-app-bar app :color="appBarColor" elevate-on-scroll>
+      <v-toolbar-title>JHM's PORTFOLIO</v-toolbar-title>
+
+      <!-- ABOUT 버튼 추가 -->
+      <v-btn @click="scrollToIntroduce">ABOUT</v-btn>
+      <v-btn @click="scrollToArchiving">Archiving</v-btn>
+      <v-btn @click="scrollToPortfolio">PROJECT</v-btn>
+
+    </v-app-bar>
+
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
+
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-
   data: () => ({
-    //
+    appBarColor: 'transparent',
   }),
-}
+  methods: {
+    // 스크롤 이벤트 핸들러
+    handleScroll() {
+      // Get the scroll position
+      const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+      // Update the app bar color based on the scroll position
+      this.appBarColor = scrollPosition > 0 ? 'white' : 'brown';
+    },
+
+    // ABOUT 버튼 클릭 시 Introduce 섹션으로 스크롤하는 메서드
+    scrollToIntroduce() {
+      const introduceSection = document.getElementById('introduce');
+      if (introduceSection) {
+        window.scrollTo({
+          top: introduceSection.offsetTop,
+          behavior: 'smooth',
+        });
+      }
+    },
+    scrollToPortfolio() {
+      const portfolioSection = document.getElementById('portfolio');
+      if (portfolioSection) {
+        window.scrollTo({
+          top: portfolioSection.offsetTop,
+          behavior: 'smooth',
+        });
+      }
+    }, scrollToArchiving() {
+      const archivingSection = document.getElementById('archiving');
+      if (archivingSection) {
+        window.scrollTo({
+          top: archivingSection.offsetTop,
+          behavior: 'smooth',
+        });
+      }
+    },
+  },
+  mounted() {
+    // 스크롤 이벤트 리스너 추가
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeDestroy() {
+    // 스크롤 이벤트 리스너 제거
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+};
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,48 +2,75 @@
   <div>
     <div id="top">
       <swiper class="swiper" :slides-per-view="1" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
-        <swiper-slide style="background-image:url(/portfolio_mjc/k.jpg)" class="slide">
+        <swiper-slide style="background-image:url(/portfolio_mjc/background.jpg)" class="slide">
 
-          <div class="header1"> |Portfolio</div>
-          <div class="header2">Myongji College </div>
-          <div class="header3">Department of Information and Communication Engineering</div>
+          <div class="header1"> Portfolio</div>
 
-          <div class="header4">Hanmin Jo </div>
+          <div class="header0">----------</div>
+
+          <div class="header2">Hanmin Jo </div>
+
 
         </swiper-slide>
         <swiper-slide style="background-image:url(/portfolio_mjc/top.1.jpg)" class="slide">
-          <div class="header">PROGRAMING</div>
+          <div class="header5">PROGRAMING</div>
         </swiper-slide>
         <swiper-slide style="background-image:url(/portfolio_mjc/d.jpg)" class="slide">
-          <div class="header">CORDING </div>
+          <div class="header6">CORDING </div>
 
         </swiper-slide>
 
       </swiper>
     </div>
     <div id="introduce">
-      <div class="header">| INTRODUCE</div>
+      <div class="header">| ABOUT</div>
       <div class="list1">
         <div class="profile">
-          <img src="/portfolio_mjc/j.jpg" alt="조한민의 사진" class="profile-image">
-          <div class="details">
-            <h2 class="name">조한민</h2>
-            <p class="college">명지전문대학</p>
-            <p class="major">정보통신공학과</p>
-            <p class="class">전공심화과정</p>
-            <p class="graduation">졸업 예정 (2024년)</p>
-            <p class="description">
-
-            </p>
+          <div class="content">
+            <img src="/portfolio_mjc/j.jpg" alt="조한민의 사진" class="profile-image">
+            <div class="details">
+              <h2 class="name">이름:조한민</h2>
+              <p class="college">학교:명지전문대학</p>
+              <p class="major">학과:정보통신공학과</p>
+              <p class="class">본과:전공심화과정</p>
+              <p class="graduation">졸업년도:2024년</p>
+              <p class="description"></p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div id="portfolio">
-      <div class="header">|PORTFOLIO</div>
+    <div id="archiving">
+      <div class="header">| ARCHIVING</div>
       <div class="list">
-        <div class="item" @click="movePage('/portfolio1')" style="background-image:url(/portfolio_mjc/car.png)"></div>
+        <div class="content">
+          <a href="https://github.com/Hanminjo" target="_blank">
+            <img src="/portfolio_mjc/GIT.png" alt="깃" class="profile-image">
+            <p class="GIT">Github</p>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div id="portfolio">
+      <div class="header">|PROJECT</div>
+      <div class="list">
+        <div class="item" @click="movePage('/portfolio1')" style="background-image:url(/portfolio_mjc/car.png)">
+          <div class="item-content">
+            <h3 class="item-title">Clean RC Car Project</h3>
+            <p class="item-description">
+              Designed and built an RC car with advanced features, including remote control, autonomous mode, and
+              obstacle avoidance.
+            </p>
+          </div>
+        </div>
         <div class="item" @click="movePage('/portfolio2')" style="background-image:url(/portfolio_mjc/AIRFOOTBALL.png)">
+          <div class="item-content">
+            <h3 class="item-title">AIR FOOTBALL Project</h3>
+            <p class="item-description">
+              Developed an exciting AIRFOOTBALL game with innovative features, including multiplayer support and
+              realistic physics.
+            </p>
+          </div>
         </div>
         <div class="item" @click="movePage('/portfolio3')" style="background-image:url(/portfolio_mjc/top.1.jpg)"></div>
         <div class="item" @click="movePage('/portfolio4')" style="background-image:url(/portfolio_mjc/top.1.jpg)"></div>
@@ -52,6 +79,9 @@
       </div>
     </div>
 
+    <div class="copyright">
+      <p>© 2023. Jo Han Min. All rights reserved.</p>
+    </div>
   </div>
 </template>
 
@@ -97,6 +127,30 @@ export default defineComponent({
 });
 </script>
 <style lang="less">
+.copyright {
+  text-align: center;
+  padding: 20px;
+  background-color: #333;
+  color: #fff;
+  font-size: 14px;
+  margin-top: 20px;
+}
+
+.github-link {
+  text-align: center;
+  margin-top: 10px;
+}
+
+.github-link a {
+  color: #ffffff;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.github-link a:hover {
+  text-decoration: underline;
+}
+
 #top {
   height: 100vh;
 
@@ -106,21 +160,60 @@ export default defineComponent({
     .slide {
       background-position: center center;
       background-size: cover;
+      opacity: 0.8;
 
-      .header {
+      .header5 {
         font-size: 160px;
         font-weight: bold;
         text-transform: uppercase;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
         /* 대문자로 변환 */
         color: #ffffff;
         /* 적절한 색상 선택 */
 
       }
 
-      .header1 {
+      .header6 {
         font-size: 160px;
         font-weight: bold;
         text-transform: uppercase;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+        /* 대문자로 변환 */
+        color: #ffffff;
+        /* 적절한 색상 선택 */
+
+      }
+
+      .header1,
+      .header2,
+      .header3,
+      .header4,
+      .header5,
+      .header6 {
+        position: relative;
+        font: 160px bold uppercase #ffffff;
+
+
+      }
+
+      .header0 {
+        font-size: 50px;
+        font-weight: bold;
+        text-align: center;
+        text-transform: uppercase;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+        /* 대문자로 변환 */
+        color: #441c1c;
+        /* 적절한 색상 선택 */
+
+      }
+
+      .header1 {
+        font-size: 190px;
+        font-weight: bold;
+        text-align: center;
+        text-transform: uppercase;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
         /* 대문자로 변환 */
         color: #ffffff;
         /* 적절한 색상 선택 */
@@ -128,28 +221,10 @@ export default defineComponent({
       }
 
       .header2 {
-        font-size: 65px;
+        font-size: 165px;
         font-weight: bold;
-        text-transform: uppercase;
-        /* 대문자로 변환 */
-        color: #ffffff;
-        /* 적절한 색상 선택 */
-
-      }
-
-      .header3 {
-        font-size: 40px;
-        font-weight: bold;
-
-        color: #ffffff;
-        /* 적절한 색상 선택 */
-
-      }
-
-      .header4 {
-        font-size: 150px;
-        font-weight: bold;
-
+        text-align: center;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
         color: #ffffff;
         /* 적절한 색상 선택 */
 
@@ -161,98 +236,117 @@ export default defineComponent({
 
 
 #introduce {
-  background-color: #f8f8f8;
-  /* 배경색 설정 */
+  background-color: #ffffff;
   padding: 50px 0;
-  /* 위아래 패딩 조절 */
   text-align: center;
 
   .header {
-    font-size: 40px;
+    font-size: 60px;
     text-align: center;
     font-weight: bold;
     text-transform: uppercase;
-    /* 대문자로 변환 */
-    color: #2e3c4e;
-    /* 적절한 색상 선택 */
-
+    color: #000000;
   }
 
-
-
-
   .list1 {
+    .profile {
+      display: flex;
+      justify-content: center;
+      /* 가로 정렬을 중앙으로 변경 */
+      align-items: center;
+      flex-direction: column;
+      /* 세로 정렬을 중앙으로 변경 */
 
-    .item {
-      .profile {
+      .content {
         display: flex;
         justify-content: center;
         align-items: center;
         flex-wrap: wrap;
+      }
 
-        .profile-image {
-          width: 150px;
-          height: 150px;
-          border-radius: 50%;
-          margin-right: 20px;
-          object-fit: cover;
-          border: 2px solid #fff;
-          /* 테두리 추가 */
+      .profile-image {
+        width: 300px;
+        height: 300px;
+        border-radius: 50%;
+        object-fit: cover;
+        border: 2px solid #fff;
+      }
+
+      .details {
+        flex: 1;
+        text-align: left;
+
+        .name {
+          font-size: 40px;
+          font-weight: bold;
+          color: #333;
         }
 
-        .details {
-          text-align: left;
+        .college,
+        .major,
+        .class,
+        .graduation {
+          font-size: 30px;
+          color: #666;
+          margin-bottom: 10px;
+        }
 
-          .name {
-            font-size: 24px;
-            font-weight: bold;
-            color: #333;
-          }
+        .graduation {
+          margin-bottom: 15px;
+        }
 
-          .college {
-            color: #666;
-            margin-bottom: 10px;
-
-          }
-
-          .major {
-            color: #666;
-            margin-bottom: 10px;
-          }
-
-          .class {
-            color: #666;
-            margin-bottom: 10px;
-          }
-
-          .graduation {
-            color: #666;
-            margin-bottom: 15px;
-          }
-
-          .description {
-            font-size: 16px;
-            color: #555;
-            line-height: 1.6;
-          }
+        .description {
+          font-size: 16px;
+          color: #555;
+          line-height: 1.6;
         }
       }
     }
   }
 }
 
-#portfolio {
+#archiving {
+  background-color: #552b0d;
+  padding: 50px 0;
+  text-align: center;
+
   .header {
-    font-size: 40px;
+    font-size: 60px;
     text-align: center;
     font-weight: bold;
     text-transform: uppercase;
-    /* 대문자로 변환 */
-    color: #2e3c4e;
-    /* 적절한 색상 선택 */
-
+    color: #ffffff;
   }
 
+  .profile-image {
+    width: 300px;
+    height: 300px;
+    border-radius: 50%;
+    object-fit: cover;
+    border: 2px solid #fff;
+  }
+
+  .GIT {
+    font-size: 19px;
+    text-align: center;
+    font-weight: bold;
+
+    color: #ffffff;
+  }
+}
+
+#portfolio {
+  background-color: #ffffff;
+  padding: 50px 0;
+  text-align: center;
+
+  .header {
+    font-size: 60px;
+    text-align: center;
+    font-weight: bold;
+    text-transform: uppercase;
+    color: #000000;
+  }
 
   width:1000px;
   margin: 0 auto;
@@ -263,20 +357,47 @@ export default defineComponent({
     grid-gap: 10px;
 
     .item {
+      position: relative;
       width: 300px;
       height: 300px;
-      /* 같은 크기로 설정 */
       background-position: center center;
       background-size: cover;
       aspect-ratio: 1/1;
       border-radius: 10px;
       transition: transform 0.3s ease;
-      /* 호버 효과를 위한 애니메이션 */
-    }
 
-    .item:hover {
-      transform: scale(1.1);
-      /* 호버 시 확대 효과 */
+      &:hover {
+        transform: scale(1.1);
+
+        .item-content {
+          opacity: 1;
+        }
+      }
+
+      .item-content {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: 20px;
+        background-color: rgba(0, 0, 0, 0.7);
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+        color: #fff;
+        opacity: 0;
+        transition: opacity 0.3s ease;
+
+      }
+
+      .item-title {
+        font-size: 18px;
+        font-weight: bold;
+        margin-bottom: 10px;
+      }
+
+      .item-description {
+        font-size: 14px;
+      }
     }
   }
 }
